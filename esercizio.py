a806ca27edcd45eee81cb39585b59a9117e2e87b
DarioValentini <dario.valentini.s02@liceoviconapoli.it>
2021-11-15 21:56:25 +0100

    Create esercizio.py

---
+++ esercizio.py
@@ -0,0 +1,10 @@
+a= input("inserisci un numero: ")
+b= input("inserisci un secondo numero: ")
+
+if a>b:
+    print("il primo è maggiore o uguale")
+elif a==b:
+    print("il primo e il secondo sono uguali")
+else:
+    print("il secondo è maggiore")
+    
