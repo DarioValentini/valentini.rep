dfc9ec97d3e0e6287da6b53619b4e4203202063c
DarioValentini <dario.valentini.s02@liceoviconapoli.it>
2022-10-24 18:58:34 +0200

    Create Conversione numerica-Valentini.py

---
+++ Conversione numerica-Valentini.py
@@ -0,0 +1,15 @@
+print ("numero di partenza = 110")
+
+Ottale = 0o110
+
+print ("In ottali= ", Ottale)
+
+Esadecimale = 0xA110C
+
+print ("In esadecimali= ", Esadecimale)
+
+Binario = 0b110
+
+print ("In Binario=", Binario)
+
+
